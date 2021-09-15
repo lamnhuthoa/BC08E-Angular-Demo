dca558340feba10bd501e77e701af0efe008f876
lamnhuthoa <hoalamnhut@gmail.com>
2021-09-15 19:22:55 +0700

    angulart b3

---
+++ src/app/Components/DirectiveModule/DemoNgContent.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-demo-ngContent',
+    template: ``
+})
+
+export class DemoNgContentComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
--- src/app/Components/DirectiveModule/Directive.module.ts
+++ src/app/Components/DirectiveModule/Directive.module.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { DemoNgContentComponent } from './DemoNgContent.component';
 import { DirectiveComponent } from './Directive.component';
 
 
 @NgModule({
     imports: [CommonModule, FormsModule],
     exports: [DirectiveComponent],
-    declarations: [DirectiveComponent]
+    declarations: [DirectiveComponent, DemoNgContentComponent]
 })
 export class DirectiveModule { }
